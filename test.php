44504014a1f7085955bcb4c2e4df8ee5ccef7f26
thehappycamden <lego76666@gmail.com>
2025-10-25 11:51:35 -0700

    Testing php setup

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Grad Student Database</title>
+    <link rel="stylesheet" src="style.css">
+</head>
+<body>
+    <h1>Grad Student Database</h1>
+    <p>
+        <ul>
+            <li> Student ID:
+                <?php 
+                    echo $_POST['studentid'];
+                ?>
+            </li>
+        </ul>
+    </p>
+</body>
+</html>
